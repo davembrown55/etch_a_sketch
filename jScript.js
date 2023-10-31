b16f4813af175827076fee0f8d106e088495dfa1
Dave Brown <davembrown55@googlemail.com>
2023-10-31 12:29:47 +0000

    basic functionality created. Grid fits on any sized screen. Validation added

--- jScript.js
+++ jScript.js
@@ -6,46 +6,51 @@ let containerStyle = window.getComputedStyle(container);
 let parentWidth = window.innerWidth; // Width of the window (parent of container)
 let parentHeight = window.innerHeight; // height of the window (parent of container)
 
+// Set gridContainer width to either 90% of screen height, or width. Depending on which is smaller, so that grid always fits on screen
 let parentValue = parentWidth < parentHeight ? parentWidth : parentHeight;
-
-// Pick up here tomorrow
-
-
-let containerWidth = parentValue.endsWith('%') ? 
-                     parseFloat((parentValue) / 100 * 90) :
-                     parseInt(containerStyle.width, 10);
-
+let containerWidth = parentValue / 100 * 90;
 container.style.width = `${containerWidth}`;
-
-
-// Set container width to be either 90% of container width or height. Depending on which is smaller. 
-// So then the all the grid will fit on screen
-
-
-
-
-// accurate container width to assist with accurate girid Cell size
-// let containerWidth = containerStyle.width.endsWith('%') ?   
-//                      parseFloat((containerStyle.width) / 100 * parentWidth) :
-//                      parseInt(containerStyle.width, 10);
-
 
 
 let gridSetBtn = document.querySelector('.gridSizeBtn');
 let usrInput = document.querySelector('.usrGridInput');
+let inputArea = document.querySelector('.inputDiv');
 
-const getGridSize = gridSetBtn.addEventListener('click', () => {
+
+
+
+gridSetBtn.addEventListener('click', () => {
     //get user input and create grid
-    const gridSize = usrInput.value;  
-    makeGrid(gridSize);    
-    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr`;
-    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr`;
-    // execute hover functionality
-    let gridCell = document.querySelectorAll('.gridCell');
-    hoverColor(gridCell);
-    // reset input
+    let gridSize = usrInput.value;  
     usrInput.value = '';
+    getGridsize(gridSize);
+
 })
+//Works when enter is pressed too
+usrInput.addEventListener('keyup',function(e){
+    if (e.keyCode === 13) {
+        let gridSize = usrInput.value;  
+        usrInput.value = '';
+        getGridsize(gridSize);  
+  }  
+});
+
+function getGridsize (gridSize) {
+    if (parseInt(gridSize, 10) > 0 && parseInt(gridSize, 10) <= 100 ) {
+        makeGrid(gridSize);    
+        container.style.gridTemplateRows = `repeat(${gridSize}, 1fr`;
+        container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr`;
+        // execute hover functionality
+        let gridCell = document.querySelectorAll('.gridCell');
+        hoverColor(gridCell);
+
+        inputArea.style.display = 'none';
+    } else {
+        alert('Please enter a number between 0 & 100');
+        gridSize = null;
+    }
+}
+
 
 function hoverColor (gridCell) {gridCell.forEach(cell => {
         cell.addEventListener('mouseover' , () => {
@@ -70,11 +75,20 @@ function makeGrid (num) {
         divGrid.style.backgroundColor = `black`;
         divGrid.style.boxSizing = `border-box`;
         divGrid.style.border = `1px solid white`;
-        container.style.padding = `${cellWidth}px 0`;
+        container.style.padding = `${cellWidth / 2}px 0`;
 
         container.appendChild(divGrid);
 
     }
 }
+
+
+
+// For working out gridContainer width when set with css 90% of parent width.
+// accurate container width to assist with accurate grid Cell size
+// let containerWidth = containerStyle.width.endsWith('%') ?   
+//                      parseFloat((containerStyle.width) / 100 * parentWidth) :
+//                      parseInt(containerStyle.width, 10);
+
 
 
